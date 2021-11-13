0483da956a244623246a722a0da54f6df33a9d9c
Dani Sanchez <danysq87@gmail.com>
2021-11-13 18:12:47 +0100

    Added routes to loginController,userRoutes, models

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
-require("dotenv").config();
+const { validate } = require("express-validation");
+
+const { userLogin, userSignUp } = require("./controllers/loginControllers");
 
 const router = express.Router();
 
